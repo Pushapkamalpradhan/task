65b7598842b91aa5789c9d35ed420b618319a24b
pushap kamal pradhan <87000188+Pushapkamalpradhan@users.noreply.github.com>
2022-04-14 22:10:14 +0530

    Add files via upload
    java script

---
+++ new/day-1-string_practice/string_practice.js
@@ -0,0 +1,121 @@
+// let txt = "amritpal singh"
+
+
+// let yourname = "kamal";
+// let length = txt.length
+// console.log(length)
+// console.log(yourname.length)
+// let part = yourname.slice(-5,-3)
+// console.log(part)
+// let fruit = "apple is red"
+// let newfruit  =fruit.replace('red', "green")
+// console.log(newfruit)
+// let yournamec =  yourname.toUpperCase()
+// console.log(yournamec)
+
+// length method 
+
+// let flower = "PUSPA"
+// let new_flower = flower.toLowerCase()
+// console.log(new_flower)
+
+
+// let st2 = "sparta"
+// let stlen = st2.length
+// console.log(stlen)
+
+// let text1 = "hello";
+// let text2 = "world";
+// let text3 = text1.concat(text2)
+// // f
+
+// console.log(text3)
+// let player = "Virat Kohli"
+// console.log(player.endsWith("i6"))
+// let yourname = "kamal"/
+// console.log(yourname.endsWith("a"))
+// console.log(yourname.startsWith("k"))
+// console.log(yourname.indexOf("a"))
+
+
+// SLICIING 
+
+// let str = "Apple, Banana, Kiwi";
+// let part = str.slice(7,12)
+
+// console.log(part)
+
+// NEGITIVE SLICING 
+// let npart = str.slice(-12,-6)
+// console.log(npart)
+
+// JAVASCRIPT STRING SUBSTRING 
+// substring puthi ni chaldi
+// let part = str.substring(7,13)
+
+
+// REPLACE STING CONTENT 
+// let text = "I am pro gamer"
+// let newText = text.replace("pro", "legend")
+// console.log(newText)
+// console.log(newText.toUpperCase)
+
+
+// UPPER AND LOWER CASE 
+// let text1 = "hello world"
+// let text2 = text1.toUpperCase()
+// console.log(text2)
+
+// let ustr = 'SPARTA'
+// let sstr  =  ustr.toLowerCase()
+// console.log(sstr)
+
+
+// STRING CONCATENATE 
+// let text1 = "hello"
+// let text2 = "world"
+// let text3 = text1.concat(" " +text2)
+// console.log(text3)
+
+// TRIM METHOD
+
+// let text1  = "           Hello world"
+// let text2 = text1.trim()
+// console.log(text2)
+
+// JAVASCRIPT STRTING CHARAT 
+// let text = "HELLO WORLD"
+// let char = text.charAt(0 , 5)
+// console.log(char)
+
+// text.split(",")
+// console.log(text)
+
+
+// JAVASCRIPT SEARCH METHODS 
+// string indexof()
+// String lastindextof()
+// string startswith()
+// String endswith ()
+
+
+// let info = "my name is amripal singh";
+// i = info.indexOf("amrit");
+// console.log(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ new/if_els/if_els.js
@@ -0,0 +1,37 @@
+// let a = 5;
+// let b = 7;
+// if(a>b){
+//     console.log("a greater")
+
+// }
+// else{
+//     console.log("b greater")
+// }
+
+// let a = 6;
+// if (a%2==0){
+//     console.log("its even number")
+// }
+// else{
+//     console.log("odd number")
+// }
+
+// let a = 5;
+// if (a>10){
+//     console.log("grater then 10")
+// }
+// else if(a<10){
+//     console.log("less then 10")
+// }
+
+// else if (a>20){
+//     console.log("a greater the 20")
+// }
+// else if (a<23){
+//     console.log("a grater then 23")
+
+// }
+
+// else{
+//     console.log("nothing match")
+// }
